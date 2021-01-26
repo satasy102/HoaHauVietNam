8c035388fe096b2f6ae5bb2d49da0a3b893c579f
Do Chanh Tin <@gmail>
2021-01-26 20:43:48 +0700

    update

--- src/main/java/com/ChanhTin/controller/ui/HomeServlet.java
+++ src/main/java/com/ChanhTin/controller/ui/HomeServlet.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
 
-@WebServlet(name = "HomeServlet", urlPatterns = "/home")
+@WebServlet(name = "HomeServlet", urlPatterns = "/")
 public class HomeServlet extends HttpServlet {
     ThiSinhService thiSinhService=new ThiSinhService();
     BaiVietService baiVietService=new BaiVietService();
